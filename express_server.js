a481eb07d66db67325dbe903a483e576837a90a4
Dom Golac <domgolac@gmail.com>
2021-05-05 03:58:25 +0000

    add edit url

--- express_server.js
+++ express_server.js
@@ -16,10 +16,6 @@ function generateRandomString() {
   return Math.random().toString(20).substr(2, 6);
 }
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -40,22 +36,19 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
-});
-
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect(`/urls/${req.params.id}`);
+});
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
